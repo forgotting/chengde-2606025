122685f0a285626d44ca90d4eb8f967b7c21a300
forgotting <forgotting@gmail.com>
2022-11-09 16:57:30 +0800

    modify

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/punch', 'App\Http\Controllers\PunchController@index')->name('punch');
+Route::get('/punch/{id}', 'App\Http\Controllers\PunchController@user_punch');
+Route::middleware('checkIp')->post('/punch/start_work/', 'App\Http\Controllers\PunchController@start');
+Route::middleware('checkIp')->post('/punch/stop_work/', 'App\Http\Controllers\PunchController@stop');
+Route::post('/punch/ajax_update', 'App\Http\Controllers\PunchController@ajaxUpdate');
+
+Route::get('/quote/{id}', 'App\Http\Controllers\QuoteController@index');
+Route::post('/quote/{id}', 'App\Http\Controllers\QuoteController@count');
+
+Route::get('excel/export/punch/{id}/{year}-{month}','ExcelController@punchExport');
+
+Route::post('/login', 'UserController@login')->name('login');
