5abee5d3cc3fc9fce074af9286105d6f439a9457
forgotting <forgotting@gmail.com>
2022-11-09 20:50:56 +0800

    modify https5

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,6 +37,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            //Https
+            \App\Http\Middleware\HttpsProtocolMiddleware::class,
         ],
 
         'api' => [
